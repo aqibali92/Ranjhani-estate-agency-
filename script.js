98a011e511bb453d25fb573a50db050a907929d0
aqibali92 <aqibalbaloch823@gmail.com>
2026-03-05 14:47:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const agencyData = {
+  name: "Ranjhani State Agency",
+  owner: "Mir Muhammad Ranjhani",
+  address: "VIP Road, Nawabshah",
+  business: "Buying & Selling of Houses, Plots, Shops & Other Properties",
+  contacts: [
+    { name: "Mir Muhammad Ranjhani", phone: "03041225743" },
+    { name: "Hakim Ali", phone: "03138198613" },
+    { name: "Aqib Ali", phone: "03193461203" },
+    { name: "Asif Ali", phone: "03191292860" }
+  ]
+};
+
+function goIndex() {
+    window.location.href = "index.html";
+}
+
+function goProperty() {
+    window.location.href = "property.html";
+}
+
+function goPlot() {
+    window.location.href = "plot.html";
+}
+function goIndex() { window.location.href = "index.html"; }
+function goProperty() { window.location.href = "property.html"; }
+function goPlot() { window.location.href = "plot.html"; }
+
